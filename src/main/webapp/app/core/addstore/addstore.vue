b03677e1e897d4b9afdf9d100f13999946c3d3bc
Irisa Kurti <irisa.kurti@hotmail.com>
2022-12-09 00:16:44 +0200

    frontend changes

--- src/main/webapp/app/core/addstore/addstore.vue
+++ src/main/webapp/app/core/addstore/addstore.vue
@@ -4,20 +4,14 @@
       <div class="col-md-8 toastify-container">
         <h1>Add store</h1>
 
-        <div class="alert alert-success" role="alert" v-if="success" v-html="$t('addstore.messages.success')">
+        <div class="alert alert-success" role="alert" v-if="success">
           <strong>Store saved!</strong> Please check your email for confirmation.
         </div>
 
-        <div class="alert alert-danger" role="alert" v-if="error" v-html="$t('addstore.messages.error.fail')">
-          <strong>Registration failed!</strong> Please try again later.
-        </div>
+        <div class="alert alert-danger" role="alert" v-if="error"><strong>Registration failed!</strong> Please try again later.</div>
 
-        <div class="alert alert-danger" role="alert" v-if="errorUserExists" v-html="$t('addstore.messages.error.userexists')">
-          <strong>Login name already registered!</strong> Please choose another one.
-        </div>
-
-        <div class="alert alert-danger" role="alert" v-if="errorEmailExists" v-html="$t('addstore.messages.error.emailexists')">
-          <strong>Email is already in use!</strong> Please choose another one.
+        <div class="alert alert-danger" role="alert" v-if="errorShopExists">
+          <strong>Shop already exists</strong> Please try another one.
         </div>
       </div>
     </div>
@@ -25,49 +19,31 @@
       <div class="col-md-8">
         <form id="addstore-form" name="addstoreForm" role="form" v-on:submit.prevent="addstore()" v-if="!success" no-validate>
           <div class="form-group">
-            <label class="form-control-label" for="username" v-text="$t('global.form[\'username.label\']')">Username</label>
+            <label class="form-control-label" for="shopname">Shop Name</label>
             <input
               type="text"
               class="form-control"
               v-model="$v.registerAccount.login.$model"
-              id="username"
-              name="login"
+              id="shopname"
+              name="shop"
               :class="{ valid: !$v.registerAccount.login.$invalid, invalid: $v.registerAccount.login.$invalid }"
               required
               minlength="1"
               maxlength="50"
-              pattern="^[a-zA-Z0-9!#$&'*+=?^_`{|}~.-]+@?[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
-              v-bind:placeholder="$t('global.form[\'username.placeholder\']')"
-              data-cy="username"
+              pattern="^[a-zA-Z0-9!#$&'*+=?^_`{|}~.-\s]+@?[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+              v-bind:placeholder="$t('global.form[\'shopname.placeholder\']')"
+              data-cy="addstore"
             />
             <div v-if="$v.registerAccount.login.$anyDirty && $v.registerAccount.login.$invalid">
-              <small
-                class="form-text text-danger"
-                v-if="!$v.registerAccount.login.required"
-                v-text="$t('register.messages.validate.login.required')"
-              >
-                Your username is required.
+              <small class="form-text text-danger" v-if="!$v.registerAccount.login.required"> Your shop's name is required. </small>
+              <small class="form-text text-danger" v-if="!$v.registerAccount.login.minLength">
+                Your shop name is required to be at least 1 character.
               </small>
-              <small
-                class="form-text text-danger"
-                v-if="!$v.registerAccount.login.minLength"
-                v-text="$t('register.messages.validate.login.minlength')"
-              >
-                Your username is required to be at least 1 character.
+              <small class="form-text text-danger" v-if="!$v.registerAccount.login.maxLength">
+                Your shop name cannot be longer than 50 characters.
               </small>
-              <small
-                class="form-text text-danger"
-                v-if="!$v.registerAccount.login.maxLength"
-                v-text="$t('register.messages.validate.login.maxlength')"
-              >
-                Your username cannot be longer than 50 characters.
-              </small>
-              <small
-                class="form-text text-danger"
-                v-if="!$v.registerAccount.login.pattern"
-                v-text="$t('register.messages.validate.login.pattern')"
-              >
-                Your username can only contain letters and digits.
+              <small class="form-text text-danger" v-if="!$v.registerAccount.login.pattern">
+                Your shop name can only contain letters and digits.
               </small>
             </div>
           </div>
